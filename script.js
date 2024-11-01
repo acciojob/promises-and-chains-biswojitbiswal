d9b22f7a68dfc1bb8dc3991c7aca4de9594de2c0
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-11-01 16:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let form = document.getElementById("form")
+let nameInput = document.getElementById("name");
+let ageInput = document.getElementById("age");
+let validate = document.getElementById("validation");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    validate.textContent = "";
+
+    if(nameInput.value === "" || ageInput.value === ""){
+        validate.textContent = "inputs cannot be empty.";
+        return;
+    }
+
+    let age = parseInt(ageInput.value);
+    let name = nameInput.value;
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(age > 18){
+                resolve(`Welcome, ${name}. You can vote.`)
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`)
+            }
+        }, 4000);
+    })
+    .then(message => alert(message))
+    .catch(error => alert(error));
+});
